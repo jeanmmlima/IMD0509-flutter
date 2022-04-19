51f0c69cd8a6d4c1dae8ed40f80058b0b414b30f
Jean Mário Moreira de Lima <jeanmariomlima@gmail.com>
2022-04-19 15:21:55 -0300

    novo componente, data e tema

--- f2_todolist/lib/main.dart
+++ f2_todolist/lib/main.dart
@@ -2,6 +2,8 @@
 
 import 'dart:math';
 
+import 'package:f2_todolist/components/TarefaForm.dart';
+import 'package:f2_todolist/components/TarefaLista.dart';
 import 'package:f2_todolist/models/tarefa.dart';
 import 'package:flutter/material.dart';
 
@@ -14,7 +16,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+      theme: ThemeData().copyWith(
+          colorScheme: ThemeData()
+              .colorScheme
+              .copyWith(primary: Colors.purple, secondary: Colors.amber),
+          textTheme: ThemeData().textTheme.copyWith(
+              headline6: TextStyle(fontWeight: FontWeight.bold, fontSize: 14))),
+    );
   }
 }
 
@@ -24,19 +34,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _tarefaController = TextEditingController();
-
-  _novaTarefa() {
-    final titulo = _tarefaController.text;
-
-    if (titulo.isEmpty) {
-      return;
-    }
-
+  _novaTarefa(String titulo, DateTime data) {
     Tarefa novaTarefa = Tarefa(
-        id: Random().nextInt(9999).toString(),
-        titulo: titulo,
-        data: DateTime.now());
+        id: Random().nextInt(9999).toString(), titulo: titulo, data: data);
 
     setState(() {
       _tarefas.add(novaTarefa);
@@ -64,59 +64,13 @@ class _MyHomePageState extends State<MyHomePage> {
         padding: EdgeInsets.all(20),
         child: Column(
           children: <Widget>[
-            TextField(
-              controller: _tarefaController,
-              decoration: InputDecoration(labelText: 'Tarefa'),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  onPressed: _novaTarefa, child: Text('Confirmar')),
-            ),
+            TarefaForm(_novaTarefa),
             SizedBox(
               height: 20,
             ),
             Column(
               children: <Widget>[
-                Container(
-                  height: 300,
-                  child: ListView.builder(
-                    itemCount: _tarefas.length,
-                    itemBuilder: (context, index) {
-                      final tarefa = _tarefas[index];
-                      return Card(
-                        child: Row(
-                          children: [
-                            Container(
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                      width: 2,
-                                      color:
-                                          tarefa.data.day == DateTime.now().day
-                                              ? Theme.of(context).primaryColor
-                                              : Theme.of(context)
-                                                  .secondaryHeaderColor),
-                                ),
-                                margin: EdgeInsets.symmetric(
-                                  horizontal: 15,
-                                  vertical: 10,
-                                ),
-                                padding: EdgeInsets.all(10),
-                                child: Text(
-                                    DateFormat('d MMM y').format(tarefa.data),
-                                    style: TextStyle(
-                                        color: tarefa.data.day ==
-                                                DateTime.now().day
-                                            ? Theme.of(context).primaryColor
-                                            : Theme.of(context)
-                                                .secondaryHeaderColor))),
-                            Text(tarefa.titulo),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                )
+                TarefaLista(_tarefas),
               ],
             )
           ],
